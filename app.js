89fec1384209600d704bf332fda6d7989d939eaa
yeged <yigityceylan@gmail.com>
2020-03-03 00:06:29 +0300

    Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5"); // LEVEL 3 SECURITY
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -24,15 +26,9 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); // LEVEL 2 SECURITY.
 
 const User = new mongoose.model("User", userSchema);
-
-
-console.log(md5("123456"));
-
-
 
 app.get("/", function(req, res){
   res.render("home");
@@ -48,32 +44,37 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }else{
-      console.log(err);
-    }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(!err){
+        res.render("secrets");
+      }else{
+        console.log(err);
+      }
+    });
   });
 });
 
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if(!err){
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }else{
-          console.log("invalid password");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(result === true){
+            res.render("secrets");
+          }else{
+            console.log("invalid password");
+          }
+        });
       }else{
         console.log("invalid email");
       }
