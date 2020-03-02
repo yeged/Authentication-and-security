eb10469b57541047a8c4b874e9f7446f82d5e287
yeged <yigityceylan@gmail.com>
2020-03-02 21:09:14 +0300

    Hashing Passwords

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -24,9 +25,12 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); // LEVEL 2 SECURITY.
 
 const User = new mongoose.model("User", userSchema);
+
+
+console.log(md5("123456"));
 
 
 
@@ -46,7 +50,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -60,7 +64,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(!err){
